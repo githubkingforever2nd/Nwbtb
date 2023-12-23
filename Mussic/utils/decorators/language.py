1c21f2f2e116999f9b1b33b4be410c66ee6806dd
githubkingforever2nd <154126061+githubkingforever2nd@users.noreply.github.com>
2023-12-23 21:30:49 +0530

    Update language.py

--- Mussic/utils/decorators/language.py
+++ Mussic/utils/decorators/language.py
@@ -1,6 +1,6 @@
 from strings import get_string
-from AarohiX.misc import SUDOERS
-from AarohiX.utils.database import (get_lang, is_commanddelete_on,
+from Mussic.misc import SUDOERS
+from Mussic.utils.database import (get_lang, is_commanddelete_on,
                                        is_maintenance)
 
 
